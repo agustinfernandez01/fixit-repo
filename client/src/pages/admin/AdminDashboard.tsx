ec24424b9b9b15172674c9d5607562aed2176622
florci <florsolis4518@gmail.com>
2026-03-23 14:11:47 -0300

    homepage

---
+++ client/src/pages/admin/AdminDashboard.tsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+	return (
+		<section style={{ textAlign: 'left' }}>
+			<h1 style={{ marginTop: 0 }}>Dashboard</h1>
+			<p>Panel de administración en construcción.</p>
+		</section>
+	)
+}
